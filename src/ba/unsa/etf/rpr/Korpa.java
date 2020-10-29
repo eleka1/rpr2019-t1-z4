799bb9be29693f2fd94799280d7d3ace0a0a0dad
Elma Leka <eleka1@etf.unsa.ba>
2020-10-29 14:50:40 +0100

    Initial commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli_korpa = new Artikl[50];
+    private int broj_korpa;
+
+
+    public boolean dodajArtikl(Artikl a) {
+        if(broj_korpa == 50) return false;
+        for (int i=0; i<50; i++){
+            if(artikli_korpa[i] == null) {
+                artikli_korpa[i] = a;
+                broj_korpa = broj_korpa + 1;
+                break;
+            }
+        }
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli_korpa;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl a = null;
+        for (int i=0; i<50; i++){
+            if(kod.equals(artikli_korpa[i].getKod())) {
+                a = artikli_korpa[i];
+                broj_korpa = broj_korpa - 1;
+                break;
+            }
+        }
+        return a;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int suma = 0;
+        for(Artikl x : artikli_korpa){
+            if(x != null) suma = suma + x.getCijena();
+        }
+        return suma;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli_sm = new Artikl[1000];
+    private int broj_sm;
+
+
+    public void dodajArtikl(Artikl novi) {
+        for(int i=0; i<1000; i++){
+            if(artikli_sm[i] == null){
+                artikli_sm[i] = novi;
+                broj_sm = broj_sm + 1;
+            }
+        }
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli_sm;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl povratni = null;
+        for(int i=0; i<1000; i++){
+            if(artikli_sm[i] == null) continue;
+            if(kod.equals(artikli_sm[i].getKod())){
+                povratni = artikli_sm[i];
+                artikli_sm[i] = null;
+                broj_sm = broj_sm - 1;
+                break;
+            }
+        }
+        return povratni;
+    }
 }
